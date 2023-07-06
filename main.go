efbf92dffcc375ec2ec9c19ecef4d7d254f95ce3
ESPIE <remi.espie@etu.umontpellier.fr>
2023-07-06 08:59:34 +0200

    fix: important events display method

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 		log.Fatalf("Error loading time")
 	}
 
-	today := time.Now().In(location)
+	today := time.Now().In(location).AddDate(0, 0, 6)
 	tomorrow := today.AddDate(0, 0, 1)
 
 	var todayCourse []*ics.VEvent
@@ -186,8 +186,13 @@ func getEmbed(course, event []*ics.VEvent, day string, weather discordwebhook.Fi
 	importantTitle := ""
 
 	if len(event) > 0 {
-		importantTitle = "⚠️ **" + strconv.Itoa(len(event)) + "** important events " + day + " !"
-		content = ""
+		if len(event) > 1 {
+			importantTitle = "⚠️ **" + strconv.Itoa(len(event)) + "** important events " + day + " !"
+		} else {
+			importantTitle = "⚠️ **1** important event " + day + " !"
+		}
+
+		importantContent := ""
 		for _, event := range event {
 			start, err := event.GetStartAt()
 			if err != nil {
@@ -197,12 +202,12 @@ func getEmbed(course, event []*ics.VEvent, day string, weather discordwebhook.Fi
 			if err != nil {
 				return discordwebhook.Embed{}
 			}
-			content += "\n**" + start.In(location).Format("15:04") + "** → **" + end.In(location).Format("15:04") + "** : " + event.GetProperty("SUMMARY").Value
+			importantContent += "\n**" + start.In(location).Format("15:04") + "** → **" + end.In(location).Format("15:04") + "** : " + event.GetProperty("SUMMARY").Value
 		}
 
 		importantField = discordwebhook.Field{
 			Name:   &importantTitle,
-			Value:  &content,
+			Value:  &importantContent,
 			Inline: nil,
 		}
 	}
